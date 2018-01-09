31b63432c79aa3721ad24f551f0c0774e726615b
ydoi <doi@mmmcorp.co.jp>
2018-01-10 00:46:18 +0900

    [P32] parse query string

--- hello.js
+++ hello.js
@@ -1,9 +1,30 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer(
-    (request, response) => {
-        response.writeHead(200, { 'Content-Type' : 'text/plain'});
-        response.end('Hello World\n');
+    (req, res) => {
+        let name = require('url').parse(req.url, true).query.name;
+
+        if (name === undefined) name = 'world';
+
+        if (name == 'burningbird') {
+            const file = 'phoenix5a.png';
+            fs.stat(file, (err, stat) => {
+                if (err) {
+                    console.error(err);
+                    res.writeHead(200, { 'Content-Type' : 'text/plain'});
+                    res.end("Sorry, Burningbird isn't around right now\n");
+                } else {
+                    let img = fs.readFileSync(file);
+                    res.contentType = 'image/img';
+                    res.contentLength = stat.size;
+                    res.end(img, 'binary');
+                }
+            })
+        } else {
+            res.writeHead(200, { 'Content-Type' : 'text/plain'});
+            res.end(`Hello ${name} \n`);
+        }
     }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
